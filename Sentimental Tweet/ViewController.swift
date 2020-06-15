c36774f2679edd7c8d13a371b8cb4a966207989f
Esraa Gamal <esraa.gamal98@gmail.com>
2020-06-15 07:47:05 +0200

    Demo UI

--- Sentimental Tweet/ViewController.swift
+++ Sentimental Tweet/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sentimentLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func predictButton(_ sender: UIButton) {
+    }
 }
 
