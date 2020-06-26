7c1c05fc23df14751aa3be157fd08efc59f73d8d
Esraa Gamal <esraa.gamal98@gmail.com>
2020-06-26 23:04:00 +0200

    added .gitignore

--- Sentimental Tweet/ViewController.swift
+++ Sentimental Tweet/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var sentimentLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
+
+    let swifter = Swifter(consumerKey: K.Api_Key, consumerSecret: K.Api_Secret_Key)
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        swifter.searchTweet(using: <#T##String#>, geocode: <#T##String?#>, lang: <#T##String?#>, locale: <#T##String?#>, resultType: <#T##String?#>, count: <#T##Int?#>, until: <#T##String?#>, sinceID: <#T##String?#>, maxID: <#T##String?#>, includeEntities: <#T##Bool?#>, callback: <#T##String?#>, tweetMode: <#T##TweetMode#>, success: <#T##Swifter.SearchResultHandler?##Swifter.SearchResultHandler?##(JSON, JSON) -> Void#>, failure: <#T##Swifter.FailureHandler##Swifter.FailureHandler##(Error) -> Void#>)
     }
 
 
